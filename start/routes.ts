913d7bbf25ce01cbab11ace64ce70497a1e70a95
georginm <georginmatheus@gmail.com>
2021-09-24 16:01:43 -0300

    chore: add routes to users and sessions

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,16 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+  // Authenticate Test Route
+  Route.get('/', async ({ auth }: HttpContextContract) => {
+    await auth.use('api').authenticate()
+    return { hello: 'world' }
+  })
+
+  // Users
+  Route.resource('/users', 'UsersController').except(['create', 'edit']).as('user')
+
+  // Session
+  Route.post('/sessions', 'SessionsController.store').as('session.store')
+}).prefix('/api')

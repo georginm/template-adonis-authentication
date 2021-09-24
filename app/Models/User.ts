d51d79bfe48e47c68450de2361749369436514d2
georginm <georginmatheus@gmail.com>
2021-09-24 14:37:54 -0300

    feat: add @adonisjs/auth

---
+++ app/Models/User.ts
@@ -0,0 +1,44 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+import { v4 as uuidv4 } from 'uuid'
+
+export default class User extends BaseModel {
+  public static table = 'users'
+
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public rememberMeToken?: string
+
+  @column()
+  public cpf: string
+
+  @column()
+  public fullname: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+
+  @beforeCreate()
+  public static idGenerate(user: User) {
+    user.id = uuidv4()
+  }
+}
